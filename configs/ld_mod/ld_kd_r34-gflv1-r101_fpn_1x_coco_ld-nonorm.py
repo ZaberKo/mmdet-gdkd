7df97afd62e17a9748d14f6aa833b0561c2fd416
zaber <20830726+ZaberKo@users.noreply.github.com>
2023-10-19 15:34:27 +0800

    add new config

--- configs/ld_mod/ld_kd_r34-gflv1-r101_fpn_1x_coco_ld-nonorm.py
+++ configs/ld_mod/ld_kd_r34-gflv1-r101_fpn_1x_coco_ld-nonorm.py
@@ -1,4 +1,4 @@
-_base_ = ['./ld_kd_r18-gflv1-r101_fpn_1x_coco.py']
+_base_ = ['./ld_kd_r34-gflv1-r101_fpn_1x_coco.py']
 
 model = dict(
     bbox_head=dict(
@@ -10,9 +10,10 @@ _base_.wandb_backend.init_kwargs.update(
     dict(
         name='{{fileBasenameNoExtension}}',
         group='{{fileBasenameNoExtension}}_group',
-        tags=['ld', 'kd', 'r18-gflv1-r101', 'fpn', '1x', 'coco', 'ld-mean']
+        tags=['ld', 'kd', 'r34-gflv1-r101', 'fpn', '1x', 'coco', 'ld-mean']
     )
 )
+
 
 vis_backends = [
     dict(type='LocalVisBackend'),
